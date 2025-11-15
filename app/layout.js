1215f2ce595ffd95a3686233ee1af8e0d8eaa54b
mission122210 <gz707985@gmail.com>
2025-11-15 10:30:34 -0500

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,19 +14,40 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Nexxen FAQ",
   description: "Find quick answers to common questions about Nexxen’s services, support hours, team policies, and more. Get the help you need or connect with our customer support team directly.",
-  image: "/meta.webp"
+  openGraph: {
+    title: "Nexxen FAQ",
+    description:
+      "Find quick answers to common questions about Nexxen’s services.",
+    images: [
+      {
+        url: "/meta.webp",
+        width: 1200,
+        height: 630,
+        alt: "Nexxen Preview Image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://faqnexxen.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={metadata.image} />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
   );
 }
+
